1f3bec550826c9468f5a0d360626a230e4f1a1d7
Javier Toribio Gómez <javitgomez@gmail.com>
2022-01-18 21:14:56 +0100

    step1

---
+++ src/test/java/actions/EmployeesActions.java
@@ -0,0 +1,102 @@
+package actions;
+
+import elements.EmployeesElements;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class EmployeesActions {
+
+    EmployeesElements employeesElements = new EmployeesElements();
+    public void openEmployees(){
+        open("C:\\Users\\javier.toribio\\Documents\\GitHub\\ui-test-asse");
+    }
+
+
+    /*public void buttonCookies(){
+        COOKIES_BUTTON.click();
+    }
+    public void buttonLoginZara(){
+        LOGON_BUTTON.click();
+    }
+    public void enterUser(){
+        USER_BAR.sendKeys("ldd39847@qopow.com");
+    }
+    public void enterPass(){
+        PASSWORD_BAR.sendKeys("8xh#qL#n");
+    }
+    public void buttonLoginConfirm(){ CONFIRM_lOGON_BUTTON.click(); }
+    public void buttonSearch(){ SEARCH_BUTTON.click(); }
+    public void enterSearch(){
+        SEARCH_BARR.sendKeys("ENERGETICALLY NEW YORK");
+    }
+    public void buttonPerfume(){ SEARCH_BUTTON_PERFUME.click(); }
+    public void buttonAddCart(){
+        BUTTON_ADD_CART.click();
+    }
+    public void buttonGoCart(){
+        BUTTON_GO_CART.click();
+    }
+    public void buttonLess1(){
+        BUTTON_LESS_1.click();
+    }
+    public void buttonEditOrder(){
+        BUTTON_EDIT_ORDER.click();
+    }
+    public void buttonGiftCard(){
+        BUTTON_GIFT_CARD.click();
+    }
+    public void enterFrom(){
+        FROM_BAR.sendKeys("Manuel Martinez");
+    }
+    public void enterMessage(){
+        MESSAGE_BAR.sendKeys("Con mucho cariño");
+    }
+    public void buttonSaveEdit(){
+        BUTTON_SAVE_EDIT.click();
+    }
+    public void buttonContinue1(){
+        BUTTON_CONTINUE_1.click();
+    }
+    public void buttonContinue2(){
+        BUTTON_CONTINUE_2.click();
+    }
+    public void buttoncard(){
+        CARD_BUTTON.click();
+    }
+    public void buttonContinue3(){
+        BUTTON_CONTINUE_3.click();
+    }
+    public void buttonBack(){
+        BUTTON_BACK.click();
+    }
+    public void buttonAccount(){
+        BUTTON_ACCOUNT.click();
+    }
+    public void buttonLogout(){
+        BUTTON_LOGOUT.click();
+    }
+    public void buttonOpenMenu(){
+        BUTTON_OPEN_MENU.click();
+    }
+    public void buttonBag(){
+        BUTTON_BAG.click();
+    }
+    public void buttonNewCollection(){
+        BUTTON_NEW_COLLECTION.click();
+    }
+    public void buttonWomen(){
+        BUTTON_WOMEN.click();
+    }
+    public void buttonSelectBag(){
+        BUTTON_SELECT_BAG.click();
+    }
+    public void buttonColor(){
+        BUTTON_COLOR.click();
+    }
+    public void buttonAddCart2(){
+        BUTTON_ADD_CART2.click();
+    }
+    /*public void buttonSize(){
+        BUTTON_SIZE.click();
+    }*/
+}
---
+++ src/test/java/steps/EmployeesSteps.java
@@ -0,0 +1,82 @@
+package steps;
+
+import actions.EmployeesActions;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import net.thucydides.core.annotations.Steps;
+
+public class EmployeesSteps {
+
+    @Steps
+    private EmployeesActions employeesActions;
+
+    @Given("^User enter$")
+    public void enter_zara(){
+        employeesActions.openZara();
+        employeesActions.buttonCookies();
+    }
+    @Given("^User click on login$")
+    public void user_access_zara()throws InterruptedException{
+        zaraActions.buttonLoginZara();
+        zaraActions.enterUser();
+        zaraActions.enterPass();
+        zaraActions.buttonLoginConfirm();
+        Thread.sleep(1000);
+    }
+
+    @When("^User goes to perfume$")
+    public void goes_search(){
+        zaraActions.buttonSearch();
+        zaraActions.enterSearch();
+        zaraActions.buttonPerfume();
+    }
+    @When("^User goes to the last purchase screen$")
+    public void goes_last_screen()throws InterruptedException{
+        zaraActions.buttonAddCart();
+        Thread.sleep(1000);
+        zaraActions.buttonGoCart();
+        Thread.sleep(1000);
+        //zaraActions.buttonEditOrder();
+        //zaraActions.buttonGiftCard();
+        //zaraActions.enterMessage();
+        //zaraActions.enterFrom();
+        //zaraActions.buttonSaveEdit();
+        zaraActions.buttonContinue1();
+        zaraActions.buttonContinue2();
+        zaraActions.buttoncard();
+        zaraActions.buttonContinue3();
+    }
+    @Then("^User logout$")
+    public void button_Logout(){
+        zaraActions.buttonBack();
+        zaraActions.buttonGoCart();
+        zaraActions.buttonLess1();
+        zaraActions.buttonAccount();
+        zaraActions.buttonLogout();
+    }
+    @When("^User goes to bag$")
+    public void button_Bag(){
+        zaraActions.buttonOpenMenu();
+        zaraActions.buttonBag();
+        zaraActions.buttonNewCollection();
+        zaraActions.buttonWomen();
+        zaraActions.buttonSelectBag();
+        zaraActions.buttonColor();
+    }
+    @When("^User goes to the last one purchase screen$")
+    public void button_last_one_screen()throws InterruptedException{
+        zaraActions.buttonAddCart2();
+        Thread.sleep(1000);
+        zaraActions.buttonGoCart();
+        Thread.sleep(1000);
+        zaraActions.buttonContinue1();
+        zaraActions.buttonContinue2();
+        zaraActions.buttoncard();
+        zaraActions.buttonContinue3();
+    }
+ /*@When("^Select color and size$")
+    public void click_color_size(){
+        zaraActions.buttonSize(); }*/
+}
+
